44ce67434ed536f3dbc1f6cf0bb5fe5682035e70
Ian Arnold <ianarnold29@gmail.com>
2025-04-21 20:39:43 -0400

    Added a modal for 'contact sale and schedule demo' created demo gif on buisness page designed and implemented a mock subscription page with a FAQ replaced footnote placeholder text added new arrivals section on index page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -11,11 +11,13 @@ import { CustomerInfoComponent } from './customer-info/customer-info.component';
 import { BusinessPortalComponent } from './business-portal/business-portal.component';
 import { TablesComponent } from './tables/tables.component';
 import { ReservationsComponent } from './reservations/reservations.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'subscription', component: SubscriptionComponent },
   { path: 'restaurants', component: RestaurantsComponent },
   { path: 'new-restaurant', component: NewRestaurantComponent, canActivate: [AuthGuard, RoleGuard], data: { expectedRoles: ['manager', 'admin'] } },
   // { path: 'new-reservation', component: NewReservationComponent },
